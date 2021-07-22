69009aa4c34e6004dce83f59069bfeed7e0ec1d7
solaris0115 <solaris0115@naver.com>
2021-07-23 01:05:20 +0900

    Folder struct redesign

---
+++ Symbolic_link.bat
@@ -0,0 +1,3 @@
+rmdir "E:\Program Files (x86)\SteamLibrary\steamapps\common\RimWorld\Mods\AdditionalVerb"
+mklink /d  "E:\Program Files (x86)\SteamLibrary\steamapps\common\RimWorld\Mods\AdditionalVerb" E:\GitProject\AdditionalVerb\AdditionalVerb
+pause
